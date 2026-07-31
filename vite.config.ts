4458ae2080d8d825972312707a6c5abc3ef1284f
MaryalaHarshitha02 <154434143+MaryalaHarshitha02@users.noreply.github.com>
2026-07-31 16:13:00 +0530

    Fixed the issuses

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
+export default defineConfig({    
   plugins: [
     react(),
     {
